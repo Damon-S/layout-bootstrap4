b721f8a835ddfd9bd1e83accbf157c1322b189ca
Damon <rasseyaniy85@gmail.com>
2020-05-08 12:52:49 +0300

    boot4

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.button[filter]').click(function () {
+        if ($(this).attr('filter') == 'all') {
+            if ($(this).attr('val') == 'off') {
+                $('.button[filter]').attr('val', 'off');
+                $(this).attr('val', 'on');
+                $('.button[filter]').removeClass('focused');
+                $(this).addClass('focused');
+                $('.portfolio__filter > div > div').show(300);
+            }
+        } else
+        if ($(this).attr('val') == 'off') {
+            $('.button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
+            $('.button[filter]').removeClass('focused');
+            $(this).addClass('focused');
+            $('.portfolio__filter > div > div').hide(300);
+            var filter = $(this).attr('filter');
+            $('.portfolio__filter > div > div[filter=' + filter + ']').show(300);
+        }
+    });
+});
